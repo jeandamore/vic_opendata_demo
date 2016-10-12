b82138dc12068d959315dd93b1ea18878bdf1dea
Jean D'Amore <jdamore@thoughtworks.com>
2016-10-12 23:27:25 +1000

    Added visibility for each layer

--- layers_wms.js
+++ layers_wms.js
@@ -5,14 +5,14 @@ function layers_wms(map) {
 		wms: {
 			url: 'https://services.land.vic.gov.au/catalogue/publicproxy/guest/dv_geoserver/wms',
 			layers: [
-				{name:'Parks and Reserves', layer:'CROWNLAND_PARKRES'},
-				{name:'Parishes', layer:'VMADMIN_PARISH_POLYGON'},
-				{name:'Townships', layer:'VMADMIN_TOWNSHIP_POLYGON'},
-				{name:'Recreation Carparks', layer:'FORESTS_RECWEB_CARPARK'},
-				{name:'Recreation Sites', layer:'FORESTS_RECWEB_SITE'},
-				{name:'Groundwater management area', layer:'WATER_GMA'},
-				{name:'Urban Development Programme - Industrial Land 2011', layer:'UDP_IND2011'},
-				{name:'Planning Schemes', layer:'VMPLAN_PLAN_OVERLAY'}
+				{name:'Parks and Reserves', layer:'CROWNLAND_PARKRES', visibility: true},
+				{name:'Parishes', layer:'VMADMIN_PARISH_POLYGON', visibility: true},
+				{name:'Townships', layer:'VMADMIN_TOWNSHIP_POLYGON', visibility: true},
+				{name:'Recreation Carparks', layer:'FORESTS_RECWEB_CARPARK', visibility: true},
+				{name:'Recreation Sites', layer:'FORESTS_RECWEB_SITE', visibility: true},
+				{name:'Groundwater management area', layer:'WATER_GMA', visibility: true},
+				{name:'Urban Development Programme - Industrial Land 2011', layer:'UDP_IND2011', visibility: true},
+				{name:'Planning Schemes', layer:'VMPLAN_PLAN_OVERLAY', visibility: false}
 			]
 		}
 	};
@@ -27,7 +27,7 @@ function layers_wms(map) {
      		transparent: true
      	}
    	);
-   	layer.setVisibility(true);
+   	layer.setVisibility(vic_opendata.wms.layers[i].visibility);
 
    	map.addLayer(layer);
 
