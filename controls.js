3d5852b886f57d3f134303de3a3aa861334cdd62
Jean D'Amore <jdamore@thoughtworks.com>
2016-10-12 22:02:14 +1000

    First commit

---
+++ controls.js
@@ -0,0 +1,41 @@
+
+function show_coords_on_click(map) {
+
+	OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {                
+                defaultHandlerOptions: {
+                    'single': true,
+                    'double': false,
+                    'pixelTolerance': 0,
+                    'stopSingle': false,
+                    'stopDouble': false
+                },
+
+                initialize: function(options) {
+                    this.handlerOptions = OpenLayers.Util.extend(
+                        {}, this.defaultHandlerOptions
+                    );
+                    OpenLayers.Control.prototype.initialize.apply(
+                        this, arguments
+                    ); 
+                    this.handler = new OpenLayers.Handler.Click(
+                        this, {
+                            'click': this.trigger
+                        }, this.handlerOptions
+                    );
+                }, 
+
+                trigger: function(e) {
+                    var lonlat = map.getLonLatFromPixel(e.xy);
+                    alert("You clicked near " + lonlat.lat + " N, " +
+                                              + lonlat.lon + " E");
+                }
+
+            });
+
+
+
+	var click = new OpenLayers.Control.Click();
+  map.addControl(click);
+  click.activate();
+
+}
---
+++ layers.js
@@ -0,0 +1,35 @@
+
+	var vic_opendata = {
+		wms: {
+			url: 'http://services.land.vic.gov.au/catalogue/publicproxy/guest/dv_geoserver/wms',
+			layers: [
+				{name:'Parks and Reserves', layer:'CROWNLAND_PARKRES'},
+				{name:'Parishes', layer:'VMADMIN_PARISH_POLYGON'},
+				{name:'Townships', layer:'VMADMIN_TOWNSHIP_POLYGON'}
+			]
+		}
+	};
+
+	var map = new OpenLayers.Vicmap.Map('map');
+
+	for(var i=0; i<vic_opendata.wms.layers.length; i++) {
+
+		var layer = new OpenLayers.Layer.WMS(
+			vic_opendata.wms.layers[i].name,
+    	vic_opendata.wms.url,
+     	{
+     		layers: vic_opendata.wms.layers[i].layer,
+     		transparent: true
+     	}
+   	);
+   	layer.setVisibility(true);
+
+   	map.addLayer(layer);
+
+	}
+
+	// Bendigo
+	var lonlat = new OpenLayers.LonLat(2435540.8444746, 2525620.5773373);
+	map.setCenter(lonlat);
+	map.zoomTo(6);
+	//show_coords_on_click(map);
