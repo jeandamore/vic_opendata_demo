6134e01adfab09bb16531e8ae3b27009d8adf0bc
Jean D'Amore <jdamore@thoughtworks.com>
2016-10-13 21:14:16 +1000

    Reduced zoom for Bendigo

--- regions.js
+++ regions.js
@@ -2,6 +2,6 @@
 
     var regions = [];
     regions['arden-macaulay'] = { lon: 2494634.3272452, lat: 2411628.5889354, zoom: 9 };
-    regions['bendigo'] = { lon: 2435310.8444746, lat: 2525200.5773373, zoom: 8 };
+    regions['bendigo'] = { lon: 2435310.8444746, lat: 2525200.5773373, zoom: 7 };
     regions['geelong'] = { lon: 2443094.7137491, lat: 2371487.2482366, zoom: 9 };
     regions['knox'] = { lon: 2522707.9979759, lat: 2401041.2656768, zoom: 8 };
